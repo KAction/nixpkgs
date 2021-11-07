6d83b8d2213027fdbb051abee5aa280e894377ec
Dmitry Bogatov <git#v1@kaction.cc>
2021-11-07 15:28:42 -0400

    python3.pkgs.attrs: build documentation

--- pkgs/development/python-modules/attrs/default.nix
+++ pkgs/development/python-modules/attrs/default.nix
@@ -1,39 +1,54 @@
-{ lib
-, callPackage
-, buildPythonPackage
-, fetchPypi
-}:
+{ lib, stdenv, callPackage, buildPythonPackage, fetchFromGitHub, furo, sphinx }:
 
-buildPythonPackage rec {
-  pname = "attrs";
+# Sphinx depends on attrs, so attempt to just add it into dependencies causes
+# loop. I tried to break the loop with build stages and overrides, but it
+# is too complicated (for me, ~kaction), so I build documentation in separate
+# derivation and attach result into the main derivation. Name of documentation
+# derivation is fudged to look indistinguishable from genuine "doc" output.
+let
   version = "21.2.0";
-
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "ef6aaac3ca6cd92904cdd0d83f629a15f18053ec84e6432106f7a4d04ae4f5fb";
+  src = fetchFromGitHub {
+    owner = "python-attrs";
+    repo = "attrs";
+    rev = version;
+    sha256 = "1bn7745ddxm4wsdzqxp1d7dvgqnzvnxjazz1g02di4nmxncxp051";
   };
 
-  outputs = [ "out" "testout" ];
+  main = buildPythonPackage rec {
+    inherit src version;
 
-  postInstall = ''
-    # Install tests as the tests output.
-    mkdir $testout
-    cp -R tests $testout/tests
-  '';
+    pname = "attrs";
+    outputs = [ "out" "testout" ];
 
-  pythonImportsCheck = [ "attr" ];
+    postInstall = ''
+      # Install tests as the tests output.
+      mkdir $testout
+      cp -R tests $testout/tests
+    '';
 
-  # pytest depends on attrs, so we can't do this out-of-the-box.
-  # Instead, we do this as a passthru.tests test.
-  doCheck = false;
+    pythonImportsCheck = [ "attr" ];
 
-  passthru.tests = {
-    pytest = callPackage ./tests.nix { };
+    # pytest depends on attrs, so we can't do this out-of-the-box.
+    # Instead, we do this as a passthru.tests test.
+    doCheck = false;
+
+    passthru.tests = { pytest = callPackage ./tests.nix { }; };
+
+    meta = with lib; {
+      description = "Python attributes without boilerplate";
+      homepage = "https://github.com/hynek/attrs";
+      license = licenses.mit;
+    };
   };
 
-  meta = with lib; {
-    description = "Python attributes without boilerplate";
-    homepage = "https://github.com/hynek/attrs";
-    license = licenses.mit;
+  doc = stdenv.mkDerivation {
+    inherit src;
+    name = main.name + "-doc";
+    nativeBuildInputs = [ sphinx furo ];
+
+    buildPhase = ''
+      make -C docs html
+    '';
   };
-}
+
+in main // { inherit doc; }
