7db6d791ac475f2f1f9f6aa18fbd4739281f0ab7
Dmitry Bogatov <git#v1@kaction.cc>
2021-12-26 01:58:15 -0400

    python3.pkgs.sphinxcontrib-blockdiag: fix missing __init__.py
    Without __init__.py import of sphinxcontrib.blockdiag failed, so this
    extension was unusable when building documentation with Sphinx.

--- pkgs/development/python-modules/sphinxcontrib-blockdiag/default.nix
+++ pkgs/development/python-modules/sphinxcontrib-blockdiag/default.nix
@@ -27,6 +27,12 @@ buildPythonPackage rec {
 
   unittestFlagsArray = [ "-s" "tests" ];
 
+  # For some reason upstream build system does not install __init__.py, so
+  # "import sphinxcontrib.blockdiag" does not work.
+  postInstall = ''
+    cp sphinxcontrib/__init__.py $out/lib/python*/site-packages/sphinxcontrib/
+  '';
+
   meta = with lib; {
     description = "Sphinx blockdiag extension";
     homepage = "https://github.com/blockdiag/sphinxcontrib-blockdiag";
