0bc9376daa286cbeb3c3bba0c09c39834f51b95e
Dmitry Bogatov <serenity@kaction.cc>
2022-09-12 08:21:35 -0400

    python3.pkgs.releases: fix compatibility with semantic_version >= 2.7
    Previously, derivation patched-out constraint on semantic_version, but did not
    address run-time error about
    
        Call either Version('1.2.3') or Version(major=1, ...).
    
    This is known issue, but upstream chooses to pin "semantic_version<2.7" as
    solution. More information in bitprophet/releases#86.

--- pkgs/development/python-modules/releases/default.nix
+++ pkgs/development/python-modules/releases/default.nix
@@ -1,6 +1,7 @@
 { buildPythonPackage
 , fetchFromGitHub
 , lib
+, fetchpatch
 , semantic-version
 , sphinx
 }:
@@ -16,6 +17,13 @@ buildPythonPackage rec {
     rev = version;
     hash = "sha256-XX2e6bjBNMun31h0sNJ9ieZE01U+PhA5JYYNOuMgD20=";
   };
+
+  patches = [
+    (fetchpatch { # PR 86 (Fix compatibility with >=python-semanticversion-2.7.0)
+      url = "https://github.com/bitprophet/releases/commit/8787236dffb7383427b3e1448ece9a5b3eaf5257.patch";
+      sha256 = "sha256-VdNxkne8mm11fHgTFDQyCxHX85f8DVaY67ZJxXLYYAQ=";
+    })
+  ];
 
   postPatch = ''
     substituteInPlace setup.py --replace "semantic_version<2.7" "semantic_version"
