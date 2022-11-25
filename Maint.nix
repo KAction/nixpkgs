a45d1e2b55a4d628eecff82099540642ec3df357
Dmitry Bogatov <serenity@kaction.cc>
2022-11-24 20:10:36 -0500

    Maint.nix: add xstow

--- Maint.nix
+++ Maint.nix
@@ -4,6 +4,7 @@ in {
   tup = pkgs.pkgsStatic.tup;
   inherit (pkgs) cachix;
 
+  inherit (pkgs.pkgsStatic) xstow;
   inherit (pkgs.pkgsStatic) htop;
   inherit (pkgs.pkgsStatic) curl;
   inherit (pkgs.pkgsStatic) exa;
