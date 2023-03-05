da479e45c16e56f8e7dafdb47a8445951cb77df1
Dmitry Bogatov <serenity@kaction.cc>
2023-03-04 22:24:16 -0500

    zig: build offline documentation

--- pkgs/development/compilers/zig/0.10.nix
+++ pkgs/development/compilers/zig/0.10.nix
@@ -11,6 +11,7 @@
 stdenv.mkDerivation rec {
   pname = "zig";
   version = "0.10.1";
+  outputs = [ "out" "doc" ];
 
   src = fetchFromGitHub {
     owner = "ziglang";
@@ -52,7 +53,18 @@ stdenv.mkDerivation rec {
     "-DZIG_TARGET_MCPU=baseline"
   ];
 
+  postBuild = ''
+    ln -sf ../doc .
+    ./zig2 build-exe ./doc/docgen.zig
+    ./docgen ./zig2 ./doc/langref.html.in ./langref.html
+  '';
+
   doCheck = true;
+
+  postInstall = ''
+    install -D -m644 -t $out/share/doc/$name/html ./langref.html
+  '';
+
   installCheckPhase = ''
     $out/bin/zig test --cache-dir "$TMPDIR" -I $src/test $src/test/behavior.zig
   '';
