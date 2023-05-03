40a45da9a80e4dd641a14d93b8522e39ca94ba56
Dmitry Bogatov <serenity@kaction.cc>
2023-05-02 20:12:48 -0400

    python3.pkgs.jinja2: build offline documentation in separate derivation
    That works around all complicated issues with infinite recursion.

--- pkgs/development/python-modules/jinja2/default.nix
+++ pkgs/development/python-modules/jinja2/default.nix
@@ -1,5 +1,6 @@
 { lib
 , stdenv
+, python
 , buildPythonPackage
 , pythonOlder
 , fetchPypi
@@ -10,33 +11,36 @@
 , pallets-sphinx-themes
 , sphinxcontrib-log-cabinet
 , sphinx-issues
-, enableDocumentation ? false
 }:
 
-buildPythonPackage rec {
+let
   pname = "Jinja2";
   version = "3.1.2";
-  outputs = [ "out" ] ++ lib.optional enableDocumentation "doc";
-
-  disabled = pythonOlder "3.7";
-
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-MTUacCpAip51laj8YVD8P0O7a/fjGXcMvA2535Q36FI=";
   };
+  meta = with lib; {
+    homepage = "https://jinja.palletsprojects.com/";
+    description = "Stand-alone template engine";
+    license = licenses.bsd3;
+    longDescription = ''
+      Jinja is a fast, expressive, extensible templating engine. Special
+      placeholders in the template allow writing code similar to Python
+      syntax. Then the template is passed data to render the final document.
+      an optional sandboxed environment.
+    '';
+    maintainers = with maintainers; [ pierron ];
+  };
 
-  patches = lib.optionals enableDocumentation [ ./patches/import-order.patch ];
+in buildPythonPackage rec {
+  inherit pname version src;
+
+  disabled = pythonOlder "3.7";
 
   propagatedBuildInputs = [
     babel
     markupsafe
-  ];
-
-  nativeBuildInputs = lib.optionals enableDocumentation [
-    sphinxHook
-    sphinxcontrib-log-cabinet
-    pallets-sphinx-themes
-    sphinx-issues
   ];
 
   # Multiple tests run out of stack space on 32bit systems with python2.
@@ -54,16 +58,34 @@ buildPythonPackage rec {
     "-p no:warnings"
   ];
 
-  meta = with lib; {
-    homepage = "https://jinja.palletsprojects.com/";
-    description = "Stand-alone template engine";
-    license = licenses.bsd3;
-    longDescription = ''
-      Jinja is a fast, expressive, extensible templating engine. Special
-      placeholders in the template allow writing code similar to Python
-      syntax. Then the template is passed data to render the final document.
-      an optional sandboxed environment.
-    '';
-    maintainers = with maintainers; [ pierron ];
+  passthru = {
+    doc = stdenv.mkDerivation {
+      # Forge look and feel of multi-output derivation as best as we can.
+      #
+      # Using 'outputs = [ "doc" ];' breaks a lot of assumptions.
+      name = "${pname}-${version}-doc";
+      inherit src pname version;
+
+      patches = [
+        # Fix import of "sphinxcontrib-log-cabinet"
+        ./patches/import-order.patch
+      ];
+
+      postInstallSphinx = ''
+        mv $out/share/doc/* $out/share/doc/python$pythonVersion-$pname-$version
+      '';
+
+      nativeBuildInputs = [
+        sphinxHook
+        sphinxcontrib-log-cabinet
+        pallets-sphinx-themes
+        sphinx-issues
+      ];
+
+      inherit (python) pythonVersion;
+      inherit meta;
+    };
   };
+
+  inherit meta;
 }
