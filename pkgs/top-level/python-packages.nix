d24100e8ca1796241e1c35852e71f9a2f408b65b
Dmitry Bogatov <serenity@kaction.cc>
2023-07-08 12:23:00 -0400

    python3.pkgs.sphinxcontrib-towncrier: init at 0.3.2a0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11743,6 +11743,8 @@ self: super: with self; {
     texLive = pkgs.texlive.combine { inherit (pkgs.texlive) scheme-small standalone pgfplots; };
   };
 
+  sphinxcontrib-towncrier = callPackage ../development/python-modules/sphinxcontrib-towncrier { };
+
   sphinxcontrib-websupport = callPackage ../development/python-modules/sphinxcontrib-websupport { };
 
   sphinxcontrib-youtube = callPackage ../development/python-modules/sphinxcontrib-youtube { };
