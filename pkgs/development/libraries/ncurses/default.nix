2c47764bda7e1652554b0989ab6540c545e7c493
Dmitry Bogatov <kaction.cc@gmail.com>
2025-06-29 20:54:27 +0000

    ncurses: fix build with gcc-15

--- pkgs/development/libraries/ncurses/default.nix
+++ pkgs/development/libraries/ncurses/default.nix
@@ -95,7 +95,8 @@ stdenv.mkDerivation (finalAttrs: {
       # which assumes that your openbsd is from the 90s, leading to a truly awful compiler/linker configuration.
       # No, autoreconfHook doesn't work.
       "--host=${stdenv.hostPlatform.config}${stdenv.cc.libc.version}"
-    ];
+    ]
+    ++ lib.optionals (stdenv.cc.isGNU && lib.versionAtLeast stdenv.cc.version "15.0") ["cf_cv_type_of_bool=bool"];
 
   # Only the C compiler, and explicitly not C++ compiler needs this flag on solaris:
   CFLAGS = lib.optionalString stdenv.hostPlatform.isSunOS "-D_XOPEN_SOURCE_EXTENDED";
